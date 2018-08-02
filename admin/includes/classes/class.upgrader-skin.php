bfcf31c52665c69d21a1ee331d9f62495d4d73f4
Eugene Jokerov <jokerov@gmail.com>
2018-08-02 19:18:57 +0300

    Вывод ошибок с сервера сборки компонентов

--- admin/includes/classes/class.upgrader-skin.php
+++ admin/includes/classes/class.upgrader-skin.php
@@ -3,5 +3,13 @@ class WCL_Upgrader_Skin extends WP_Upgrader_Skin {
 	public function feedback($string) {
 		// @note: Keep it empty.
 	}
+	
+	 public function header() {
+		 
+	 }
+	 
+	 public function footer() { 
+		 
+	 }
 
 }
