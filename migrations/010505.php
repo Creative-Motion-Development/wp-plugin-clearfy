53d009284f7e2efe06a47b3d6bcc284b745f3ffe
alex.kovalevv <alex.kovalevv@gmail.com>
2019-10-03 20:05:34 +0300

    Добавил очистку данных для Hide my wp, улучшил миграцию для менеджера скриптов.

--- migrations/010505.php
+++ migrations/010505.php
@@ -17,6 +17,33 @@ class WCLUpdate010505 extends Wbcr_Factory000_Update {
 
 		$wupm_updates = new WGZUpdate010108( $this->plugin );
 		$wupm_updates->install();
+
+		/**
+		 * Очищаем старые данных от плагина Hide my wp
+		 */
+		$this->clean_hide_my_wp_data();
+	}
+
+	/**
+	 * Безопасно отключаем Hide my wp, так как он будет удален.
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 */
+	private function clean_hide_my_wp_data() {
+		$is_hide_my_wp_active = get_option( $this->plugin->getPrefix() . 'hide_my_wp_activate' );
+
+		if ( is_multisite() || ! $is_hide_my_wp_active ) {
+			return;
+		}
+
+		if ( $this->is_apache() ) {
+			$this->reset_hide_my_wp_settings();
+
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+			require_once ABSPATH . 'wp-admin/includes/misc.php';
+			flush_rewrite_rules( true );
+		}
 	}
 
 	/**
@@ -103,5 +130,221 @@ class WCLUpdate010505 extends Wbcr_Factory000_Update {
 
 		WCL_Plugin::app()->deletePopulateOption( 'freemius_addons' );
 		WCL_Plugin::app()->deletePopulateOption( 'freemius_addons_last_update' );
+	}
+
+	// For Clean Hide my wp settings
+	// ============================================================================================
+
+	public static function is_hide_mode_active() {
+		return WCL_Plugin::app()->getPopulateOption( 'hide_my_wp_activate', false );
+	}
+
+	/**
+	 * Return if the server run Apache
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 *
+	 * @return bool
+	 */
+	private function is_apache() {
+		$is_apache = ( strpos( $_SERVER['SERVER_SOFTWARE'], 'Apache' ) !== false || strpos( $_SERVER['SERVER_SOFTWARE'], 'LiteSpeed' ) !== false );
+
+		return $is_apache;
+	}
+
+	/**
+	 * Get rules marker
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 *
+	 * @return string
+	 */
+	private function get_rules_marker() {
+		if ( is_multisite() ) {
+			return "Webcraftic Hide My Wp Site" . get_current_blog_id();
+		}
+
+		return "Webcraftic Hide My Wp";
+	}
+
+	/**
+	 * Return the server home path
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 * @return bool|string
+	 */
+	private function get_home_path() {
+		$home    = set_url_scheme( get_option( 'home' ), 'http' );
+		$siteurl = set_url_scheme( get_option( 'siteurl' ), 'http' );
+
+		if ( ! empty( $home ) && 0 !== strcasecmp( $home, $siteurl ) ) {
+			$wp_path_rel_to_home = str_ireplace( $home, '', $siteurl ); /* $siteurl - $home */
+			$pos                 = strripos( str_replace( '\\', '/', $_SERVER['SCRIPT_FILENAME'] ), trailingslashit( $wp_path_rel_to_home ) );
+
+			if ( $pos !== false ) {
+				$home_path = substr( $_SERVER['SCRIPT_FILENAME'], 0, $pos );
+				$home_path = trim( $home_path, '/\\' ) . DIRECTORY_SEPARATOR;;
+			} else {
+				$wp_path_rel_to_home = DIRECTORY_SEPARATOR . trim( $wp_path_rel_to_home, '/\\' ) . DIRECTORY_SEPARATOR;
+
+				$real_apth = realpath( ABSPATH ) . DIRECTORY_SEPARATOR;
+
+				$pos       = strpos( $real_apth, $wp_path_rel_to_home );
+				$home_path = substr( $real_apth, 0, $pos );
+				$home_path = trim( $home_path, '/\\' ) . DIRECTORY_SEPARATOR;
+			}
+		} else {
+			$home_path = ABSPATH;
+		}
+
+		$home_path = trim( $home_path, '\\/ ' );
+
+		//not for windows
+		if ( DIRECTORY_SEPARATOR != '\\' ) {
+			$home_path = DIRECTORY_SEPARATOR . $home_path;
+		}
+
+		return $home_path;
+	}
+
+	/**
+	 * Return whatever the htaccess config file is writable
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 * @return bool
+	 */
+	private function is_writable_htaccess_config_file() {
+		$home_path     = $this->get_home_path();
+		$htaccess_file = $home_path . DIRECTORY_SEPARATOR . '.htaccess';
+
+		if ( ( ! file_exists( $htaccess_file ) && $this->is_permalink() ) || is_writable( $htaccess_file ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param $filename
+	 * @param $markers
+	 *
+	 * @return bool
+	 */
+	private function clean_rules_in_file( $filename, $markers ) {
+
+		if ( ! file_exists( $filename ) ) {
+			if ( ! is_writable( dirname( $filename ) ) ) {
+				return false;
+			}
+			if ( ! touch( $filename ) ) {
+				return false;
+			}
+		} else if ( ! is_writeable( $filename ) ) {
+			return false;
+		}
+
+		$start_marker = $markers['start'];
+		$end_marker   = $markers['end'];
+
+		$fp = fopen( $filename, 'r+' );
+		if ( ! $fp ) {
+			return false;
+		}
+
+		// Attempt to get a lock. If the filesystem supports locking, this will block until the lock is acquired.
+		flock( $fp, LOCK_EX );
+
+		$lines = [];
+		while( ! feof( $fp ) ) {
+			$lines[] = rtrim( fgets( $fp ), "\r\n" );
+		}
+
+		// Split out the existing file into the preceding lines, and those that appear after the marker
+		$pre_lines    = $post_lines = $existing_lines = [];
+		$found_marker = $found_end_marker = false;
+		foreach ( $lines as $line ) {
+			if ( ! $found_marker && false !== strpos( $line, $start_marker ) ) {
+				$found_marker = true;
+				continue;
+			} else if ( ! $found_end_marker && false !== strpos( $line, $end_marker ) ) {
+				$found_end_marker = true;
+				continue;
+			}
+			if ( ! $found_marker ) {
+				$pre_lines[] = $line;
+			} else if ( $found_marker && $found_end_marker ) {
+				$post_lines[] = $line;
+			} else {
+				$existing_lines[] = $line;
+			}
+		}
+
+		// Generate the new file data
+		if ( $found_marker && $found_end_marker ) {
+			$new_file_data = implode( "\n", array_merge( $pre_lines, $post_lines ) );
+
+			// Write to the start of the file, and truncate it to that length
+			fseek( $fp, 0 );
+			$bytes = fwrite( $fp, $new_file_data );
+			if ( $bytes ) {
+				ftruncate( $fp, ftell( $fp ) );
+			}
+			fflush( $fp );
+			flock( $fp, LOCK_UN );
+			fclose( $fp );
+
+			return (bool) $bytes;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 */
+	private function reset_hide_my_wp_settings() {
+		WCL_Plugin::app()->deletePopulateOption( 'hide_my_wp_activate' );
+		WCL_Plugin::app()->deletePopulateOption( 'replace_content_filters' );
+		WCL_Plugin::app()->deletePopulateOption( 'replace_content_patterns' );
+		WCL_Plugin::app()->deletePopulateOption( 'recovery_content_patterns' );
+		WCL_Plugin::app()->deletePopulateOption( 'recovery_content_filters' );
+		WCL_Plugin::app()->deletePopulateOption( 'nginx_rewrite_rules' );
+		WCL_Plugin::app()->deletePopulateOption( 'apache_rewrite_rules' );
+		WCL_Plugin::app()->deletePopulateOption( 'server_configuration_error' );
+
+		//check if .htaccess file exists and is writable
+		if ( $this->is_writable_htaccess_config_file() ) {
+			$home_path     = $this->get_home_path();
+			$htaccess_file = $home_path . DIRECTORY_SEPARATOR . '.htaccess';
+
+			$markers = [
+				'start' => '# BEGIN ' . $this->get_rules_marker(),
+				'end'   => '# END ' . $this->get_rules_marker()
+			];
+			$this->clean_rules_in_file( $htaccess_file, $markers );
+		}
+	}
+
+	/**
+	 * Is permalink enabled?
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 * @since  1.6.0
+	 * @return bool
+	 * @global WP_Rewrite $wp_rewrite
+	 */
+	private function is_permalink() {
+		global $wp_rewrite;
+
+		if ( ! isset( $wp_rewrite ) || ! is_object( $wp_rewrite ) || ! $wp_rewrite->using_permalinks() ) {
+			return false;
+		}
+
+		return true;
 	}
 }
